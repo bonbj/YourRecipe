c01580b68850160770a2632bb6942dae53e255bb
jumelissa <julianamelissa42@gmail.com>
2020-05-11 18:06:05 -0300

    wip: tabs

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <main className="main">
-      
+      <h1>Nada</h1>
     </main>
   );
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Ola</h1>
+        </div>
+    )
+}
